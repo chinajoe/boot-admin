391ca2926ed1623bcc571ae25600e04d2846cdd1
qiaojinfeng3 <qiaojinfeng3@jd.com>
2022-04-20 20:09:06 +0800

    feat(admin): 课程准备、上传

---
+++ src/main/java/com/hb0730/boot/admin/project/course/orchestrate/controller/CourseOrchestrateController.java
@@ -0,0 +1,19 @@
+package com.hb0730.boot.admin.project.course.orchestrate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description: 课程编排
+ * @author: qiaojinfeng3
+ * @date: 2022/4/20 20:01
+ */
+@Slf4j
+@RestController
+@RequestMapping("")
+public class CourseOrchestrateController {
+    // todo 1. 创建课程
+    // todo 2. 编辑课程内容：存到本系统的oss上
+    // todo 3. 上传课程：开始上传素材到第三方平台，
+}
---
+++ src/main/java/com/hb0730/boot/admin/project/course/prepare/controller/CoursePrepareController.java
@@ -0,0 +1,16 @@
+package com.hb0730.boot.admin.project.course.prepare.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description: 课程准备
+ * @author: qiaojinfeng3
+ * @date: 2022/4/20 20:02
+ */
+@Slf4j
+@RestController
+@RequestMapping("")
+public class CoursePrepareController {
+}
