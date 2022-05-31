b979bbf418a634f2ed30c3dbd4656a0f91e1eecb
qiaojinfeng3 <956898518@qq.com>
2022-05-31 11:19:54 +0800

    refactor: 去除敏感信息2

--- src/main/java/com/hb0730/boot/admin/configuration/properties/CosProperties.java
+++ src/main/java/com/hb0730/boot/admin/configuration/properties/CosProperties.java
@@ -1,14 +1,23 @@
 package com.hb0730.boot.admin.configuration.properties;
 
+import com.hb0730.commons.lang.codec.Base64Utils;
 import lombok.Data;
+
+import java.nio.charset.StandardCharsets;
 
 /**
  * The type Cos properties.
  */
 @Data
 public class CosProperties {
+    private static final String SECRET_ID = "QUtJREVLRXVNWlhIcDlGRjVxTmtzbVJyMnpoOVFTRWwzN3dX";
+    private static final String SECRET_KEY = "T2JaSW0yNXRTbnh1YzVKSTNacHRpSWVtZ0xsd3doRFU=";
     private String bucketName;
     private String region;
-    private String secretId = "AKIDEKEuMZXHp9FF5qNksmRr2zh9QSEl37wW";
-    private String secretKey = "ObZIm25tSnxuc5JI3ZptiIemgLlwwhDU";
+    private String secretId = new String(
+        Base64Utils.decode(SECRET_ID.getBytes(StandardCharsets.UTF_8))
+    );
+    private String secretKey = new String(
+        Base64Utils.decode(SECRET_KEY.getBytes(StandardCharsets.UTF_8))
+    );
 }
