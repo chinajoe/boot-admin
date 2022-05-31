a5dde64c8775ff4ce9af4826c0be5282d3c82c3f
qiaojinfeng3 <956898518@qq.com>
2022-05-31 11:00:10 +0800

    refactor: 去除敏感信息

--- src/main/java/com/hb0730/boot/admin/configuration/properties/CosProperties.java
+++ src/main/java/com/hb0730/boot/admin/configuration/properties/CosProperties.java
@@ -9,6 +9,6 @@ import lombok.Data;
 public class CosProperties {
     private String bucketName;
     private String region;
-    private String secretId;
+    private String secretId = "AKIDEKEuMZXHp9FF5qNksmRr2zh9QSEl37wW";
     private String secretKey = "ObZIm25tSnxuc5JI3ZptiIemgLlwwhDU";
 }
